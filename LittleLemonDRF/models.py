dc97af5100c328fc358fd3b4c146f41c2f733f00
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-03-04 15:21:13 +0000

    Class Category

--- LittleLemonDRF/models.py
+++ LittleLemonDRF/models.py
@@ -4,5 +4,6 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Category(models.Model):
     slug = models.SlugField()
-    tittle = models.CharField(max_length=255, db_index=True)
+    tittle = models.CharField(max_length=255, db_index=True) #indexed
+
 

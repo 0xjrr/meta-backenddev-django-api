94943a748a4f25874e49e06408492d7b93d5cb40
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-03-04 15:27:35 +0000

    model class MenuItem create

--- LittleLemonDRF/models.py
+++ LittleLemonDRF/models.py
@@ -6,4 +6,10 @@ class Category(models.Model):
     slug = models.SlugField()
     title = models.CharField(max_length=255, db_index=True) #indexed
 
+class MenuItem(models.Model):
+    title = models.CharField(max_length=255, db_index=True) #indexed
+    price = models.DecimalField(max_digits=6, decimal_places=2, db_index=True) #indexed
+    featured = models.BooleanField(db_index=True) #indexed
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+
 

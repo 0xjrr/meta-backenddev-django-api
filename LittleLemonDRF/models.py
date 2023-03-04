17547a772e6664c7de2aaebdaafb9529b19d8765
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-03-04 15:22:59 +0000

    spelling mistake title

--- LittleLemonDRF/models.py
+++ LittleLemonDRF/models.py
@@ -4,6 +4,6 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Category(models.Model):
     slug = models.SlugField()
-    tittle = models.CharField(max_length=255, db_index=True) #indexed
+    title = models.CharField(max_length=255, db_index=True) #indexed
 
 

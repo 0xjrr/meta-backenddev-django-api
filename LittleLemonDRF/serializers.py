805bf1a61cd7da4f593c87e36cc430c7cb4fecf2
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-06-22 20:27:34 +0100

    add MenuItemSerializer update() method

--- LittleLemonDRF/serializers.py
+++ LittleLemonDRF/serializers.py
@@ -17,3 +17,16 @@ class MenuItemSerializer(serializers.ModelSerializer):
         category_data = validated_data.pop('category')
         category, _ = Category.objects.get_or_create(**category_data)
         return MenuItem.objects.create(category=category, **validated_data)
+
+    def update(self, instance, validated_data):
+        category_data = validated_data.pop('category', None)
+        if category_data is not None:
+            category, _ = Category.objects.get_or_create(**category_data)
+            instance.category = category
+
+        instance.title = validated_data.get('title', instance.title)
+        instance.price = validated_data.get('price', instance.price)
+        instance.featured = validated_data.get('featured', instance.featured)
+        instance.save()
+
+        return instance

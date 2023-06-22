2b0a5c9870fe43b4009c3b8c0451d79855e2bc24
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-06-22 12:26:20 +0100

    add obtain_auth_token

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('LittleLemonDRF.urls')),
     path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.authtoken'))
+    path('auth/', include('djoser.urls.authtoken')),
+    path('token/login/', obtain_auth_token),
 ]

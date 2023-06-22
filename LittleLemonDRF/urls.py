511d9282930b39a89510b21a15feb2eca9b80e38
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-06-22 12:27:31 +0100

    add MenuItems route

--- LittleLemonDRF/urls.py
+++ LittleLemonDRF/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = [
-    path('', views.test)
-]
+router = DefaultRouter(trailing_slash=False)
+router.register('menu-items', views.MenuItems, 'menu-items')
+
+urlpatterns = router.urls

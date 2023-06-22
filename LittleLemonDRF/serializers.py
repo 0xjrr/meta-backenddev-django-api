30bb0bfb76872f2881be472659f0d9fb2644dd8f
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-06-22 18:00:39 +0100

    add  CategorySerializer and MenuItemSerializer

--- LittleLemonDRF/serializers.py
+++ LittleLemonDRF/serializers.py
@@ -1,3 +1,19 @@
 from rest_framework import serializers
 from .models import * # temporary, change at end
 
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['slug', 'title']
+
+class MenuItemSerializer(serializers.ModelSerializer):
+    category = CategorySerializer()
+
+    class Meta:
+        model = MenuItem
+        fields = ['title', 'price', 'featured', 'category']
+
+    def create(self, validated_data):
+        category_data = validated_data.pop('category')
+        category, _ = Category.objects.get_or_create(**category_data)
+        return MenuItem.objects.create(category=category, **validated_data)

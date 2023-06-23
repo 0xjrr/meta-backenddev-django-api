21596ba3a6fc0e7be59c412d8a9bcb371b2f4553
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-06-23 23:16:12 +0100

    add routes manager and delivery-crew users

--- LittleLemonDRF/urls.py
+++ LittleLemonDRF/urls.py
@@ -5,5 +5,7 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter(trailing_slash=False)
 router.register(r'menu-items', views.MenuItems, basename='menu-items')
 router.register(r'cart/menu-items', views.CartViewSet, basename='cart')
+router.register(r'groups/manager/users', views.ManagerGroupViewSet, basename='manager-group')
+router.register(r'groups/delivery-crew/users', views.DeliveryCrewGroupViewSet, basename='delivery-crew-group')
 
 urlpatterns = router.urls

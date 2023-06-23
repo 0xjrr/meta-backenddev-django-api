16c5b54875a246c91e3306734ae9755135d92dc8
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-06-23 16:56:38 +0100

    remove whitespacing

--- LittleLemonDRF/views.py
+++ LittleLemonDRF/views.py
@@ -7,19 +7,19 @@ from .serializers import * # temporary, change at end
 
 
 class MenuItems(viewsets.ViewSet):
-	
+
     def list(self, request):
         queryset = MenuItem.objects.all()
         serializer = MenuItemSerializer(queryset, many=True)
         return Response(serializer.data)
-	
+
     def create(self, request):
         serializer = MenuItemSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-	
+
     def update(self, request, pk=None):
         queryset = MenuItem.objects.all()
         item = get_object_or_404(queryset, pk=pk)
@@ -34,7 +34,7 @@ class MenuItems(viewsets.ViewSet):
         item = get_object_or_404(queryset, pk=pk)
         serializer = MenuItemSerializer(item)
         return Response(serializer.data)
-	
+
     def partial_update(self, request, pk=None):
         queryset = MenuItem.objects.all()
         item = get_object_or_404(queryset, pk=pk)

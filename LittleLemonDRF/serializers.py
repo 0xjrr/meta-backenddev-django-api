ab2d65c79343aae8ea114d327e7940bb4ad0f1b0
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-06-24 22:01:21 +0100

    add read_only_fields

--- LittleLemonDRF/serializers.py
+++ LittleLemonDRF/serializers.py
@@ -61,3 +61,4 @@ class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = '__all__'
+        read_only_fields = ['status', 'date', 'total', 'user', 'delivery_crew']
